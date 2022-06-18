3d2ef6f1842b55fd8d3ad2e7606844897268d0b4
jonathanl-telenav <jonathanl@telenav.com>
2022-06-18 10:46:53 -0600

    🍱 remove old documentation due to broken image links from asset relocation

--- docs/1.2.2/javadoc/cactus-build/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"cactus.metadata","l":"com.telenav.cactus.metadata"}];updateSearchResults();
--- docs/1.4.0/javadoc/cactus-build/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.telenav.cactus.metadata","l":"BuildMetadata"},{"p":"com.telenav.cactus.metadata","l":"BuildMetadataUpdater"},{"p":"com.telenav.cactus.metadata","l":"BuildName"},{"p":"com.telenav.cactus.metadata","l":"BuildMetadata.Type"}];updateSearchResults();
